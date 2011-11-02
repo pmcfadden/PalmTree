1e6d93fe4cd0c54d6a595ee123ea94202e22e200
Patrick McFadden <pmcfadde@thoughtworks.com>
2011-11-02 11:29:16 -0500

    First commit, with project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PalmTree::Application.config.secret_token = '7943342460ba0f3d2780c1f032331e763c04904dcf1c788e8a16855be7e286d9d4d0ac44fbfe3c9d6ee3af740f16d3f05df2a055b37ab1f4a01ad64530b2a9d9'
